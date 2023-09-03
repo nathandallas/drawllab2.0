51a9ee432882156ca6073059ed8b4a4262f8049a
nathandallas <87546582+alysurr@users.noreply.github.com>
2023-09-02 20:02:34 -0400

    added server and socket.io integration

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,52 +1,75 @@
 'use client';
 
-import { FunctionComponent, useState } from 'react';
+import { FunctionComponent, useState, useEffect } from 'react';
 import { useDraw } from '@/hooks/useDraw';
-import { start } from 'repl';
-import { SketchPicker } from 'react-color';
+import { drawLine } from '@/utils/drawLine';
+import { HexColorPicker, HexColorInput } from 'react-colorful';
+import { io } from 'socket.io-client';
+const socket = io('http://localhost:3001');
 
 interface pageProps {}
 
 const page: FunctionComponent<pageProps> = ({}) => {
-	const [color, setColor] = useState<string>('#000');
-	const { canvasRef, onMouseDown, clear } = useDraw(
-		drawLine
-	);
+	const [color, setColor] = useState<string>('#000000');
+	const { canvasRef, onMouseDown, clear } = useDraw(createLine);
 
-	function drawLine({
-		prevPoint,
-		currentPoint,
-		ctx,
-	}: Draw) {
-		const { x: currX, y: currY } = currentPoint;
-		const lineColor = color;
-		const lineWidth = 5;
+	useEffect(() => {
+		const ctx = canvasRef.current?.getContext('2d')
 
-		let startPoint = prevPoint ?? currentPoint;
-		ctx.beginPath();
-		ctx.lineWidth = lineWidth;
-		ctx.strokeStyle = lineColor;
-		ctx.moveTo(startPoint.x, startPoint.y);
-		ctx.lineTo(currX, currY);
-		ctx.stroke();
+		socket.emit('client-ready')
 
-		ctx.fillStyle = lineColor;
-		ctx.beginPath();
-		ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
-		ctx.fill();
+		socket.on('get-canvas-state', () => {
+			if (!canvasRef.current?.toDataURL()) return
+			socket.emit('canvas-state', canvasRef.current.toDataURL())
+		})
+
+		socket.on('canvas-state-from-server', (state: string) => {
+			console.log('I received the state')
+			const img = new Image()
+			img.src = state
+			img.onload = () => {
+			  ctx?.drawImage(img, 0, 0)
+			}
+		  })
+	  
+		socket.on(
+			'draw-line',
+			({ prevPoint, currentPoint, color }: DrawLineProps) => {
+				if (!ctx) return;
+				drawLine({ prevPoint, currentPoint, ctx, color });
+			}
+		);
+
+		socket.on('clear', clear)
+
+		return () => {
+			socket.off('draw-line')
+			socket.off('get-canvas-state')
+			socket.off('canvas-state-from-server')
+			socket.off('clear')
+		  }
+		  
+	}, [canvasRef]);
+
+	function createLine({ prevPoint, currentPoint, ctx }: Draw) {
+		socket.emit('draw-line', { prevPoint, currentPoint, color });
+		drawLine({ prevPoint, currentPoint, ctx, color });
 	}
 
 	return (
 		<div className='w-screen h-screen bg-white flex'>
-			<div className='flex flex-col gap-10 pr-10'>
-				<SketchPicker
-					presetColors={[]}
+			<div className='flex flex-col gap-10 px-10 w-80 items-center'>
+				<HexColorPicker color={color} onChange={setColor} className='' />
+				<HexColorInput
 					color={color}
-					onChange={(e) => setColor(e.hex)}
+					onChange={setColor}
+					className='p-2 border border-black rounded-md'
+					prefixed
+					alpha
 				/>
 				<button
 					type='button'
-					onClick={clear}
+					onClick={() => socket.emit('clear')}
 					className='p-2 rounded-md border border-black'
 				>
 					Clear
